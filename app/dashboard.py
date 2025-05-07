dc2802b7da082d782d41b8f463e3b1e2a311ab91
Andy Wu <wuandy6418@gmail.com>
2025-05-07 16:22:58 -0400

    feat: add multi-indicator support with dropdown selector in dashboard

--- app/dashboard.py
+++ app/dashboard.py
@@ -6,16 +6,46 @@ import sqlite3
 
 app = dash.Dash(__name__)
 
-conn = sqlite3.connect('data/macro_data.db')
-df = pd.read_sql('SELECT * FROM cpi_data', conn)
-conn.close()
+def get_indicators():
+    conn = sqlite3.connect('data/macro_data.db')
+    indicators = pd.read_sql('SELECT DISTINCT indicator FROM macro_data', conn)['indicator'].tolist()
+    conn.close()
+    return indicators
 
-fig = px.line(df, x='date', y='CPI', title='CPI Over Time')
+def get_data_for_indicator(indicator):
+    conn = sqlite3.connect('data/macro_data.db')
+    df = pd.read_sql(f"SELECT date, value FROM macro_data WHERE indicator = ?", conn, params=(indicator,))
+    conn.close()
+    return df
 
-app.layout = html.Div([
-    html.H1('CPI Over Time'),
-    dcc.Graph(figure=fig)
-])
+indicators = get_indicators()
+
+def serve_layout():
+    return html.Div([
+        html.H1('FRED Macro Dashboard'),
+        dcc.Dropdown(
+            id='indicator-dropdown',
+            options=[{'label': ind, 'value': ind} for ind in indicators],
+            value=indicators[0] if indicators else None
+        ),
+        dcc.Graph(id='macro-graph')
+    ])
+
+app.layout = serve_layout
+
+from dash.dependencies import Input, Output
+
+@app.callback(
+    Output('macro-graph', 'figure'),
+    Input('indicator-dropdown', 'value')
+)
+def update_graph(selected_indicator):
+    if not selected_indicator:
+        return {}
+    df = get_data_for_indicator(selected_indicator)
+    fig = px.line(df, x='date', y='value', title=f'{selected_indicator} Over Time')
+    return fig
+
     
 if __name__ == '__main__':
     app.run(debug=True)
--- src/etl.py
+++ src/etl.py
@@ -8,17 +8,28 @@ load_dotenv()
 
 fred = Fred(api_key=os.getenv("FRED_API_KEY"))
 
-def get_cpi_data():
-    df = fred.get_series('CPIAUCSL').to_frame(name='CPI')
-    df.index.name = 'date'
-    df.reset_index(inplace=True)
-    return df
+INDICATORS = {
+    'CPI': 'CPIAUCSL',
+    'Unemployment Rate': 'UNRATE',
+    'Fed Funds Rate': 'FEDFUNDS'
+}
+
+def fetch_fred_data():
+    dfs = []
+    for name, series_id in INDICATORS.items():
+        data = fred.get_series(series_id).to_frame(name='value')
+        data.index.name = 'date'
+        data.reset_index(inplace=True)
+        data['indicator'] = name
+        dfs.append(data)
+    all_data = pd.concat(dfs, ignore_index=True)
+    return all_data[['date', 'indicator', 'value']]
 
 def save_to_db(df):
     conn = sqlite3.connect('data/macro_data.db')
-    df.to_sql('cpi_data', conn, if_exists='replace', index=False)
+    df.to_sql('macro_data', conn, if_exists='replace', index=False)
     conn.close()
 
 if __name__ == '__main__':
-    df = get_cpi_data()
+    df = fetch_fred_data()
     save_to_db(df)
